691bb1fdd1929a21b0b10eb0a2f397fc5bcff4b7
Noblesse68 <132678684+Noblesse68@users.noreply.github.com>
2023-05-11 15:21:58 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,16 +2,17 @@ let angle = 0
 input.calibrateCompass()
 basic.forever(function () {
     angle = input.compassHeading()
-    if (angle < 45) {
-        basic.showString("N")
-    }
-    if (angle < 135) {
+    if (angle > 45 && angle <= 135) {
         basic.showString("E")
-    }
-    if (angle < 225) {
-        basic.showString("S")
-    }
-    if (angle < 315) {
-        basic.showString("O")
+    } else {
+        if (angle > 135 && angle <= 225) {
+            basic.showString("S")
+        } else {
+            if (angle > 225 && angle <= 315) {
+                basic.showString("O")
+            } else {
+                basic.showString("N")
+            }
+        }
     }
 })
